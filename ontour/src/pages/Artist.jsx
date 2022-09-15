7ba8b51f3a9fd3951fe11975d9d7d33a52cade53
Matthew Wilson <Matt@MacBook-Pro-10.lan>
2022-09-14 22:00:32 -0700

    edit artists

--- ontour/src/pages/Artist.jsx
+++ ontour/src/pages/Artist.jsx
@@ -2,14 +2,7 @@ import React from "react";
 import '../index.css';
 function Artist() {
   return (
-    <div className="about">
-
-          {/* <div class="mt-5">
-            <h1 class="font-weight-light">Artist</h1>
-            <p>
-              This is the artist page
-            </p>
-          </div> */}
+    <div className="artist">
           <div id="artist-background" class="jumbotron bg-cover text-white">
             <div class="container py-5 text-center">
               <h1 class="display-4 font-weight-bold">The Weeknd</h1>
@@ -17,6 +10,9 @@ function Artist() {
             </div>
           </div> 
 
+          <div class="container py-5">
+            <h2 class="h3 font-weight-bold">Photo Gallery</h2>
+          </div>
     </div>
   );
 }
