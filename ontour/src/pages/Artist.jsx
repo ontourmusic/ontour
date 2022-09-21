808abf730fd1300a1a122b585a243d7624fd851d
Matthew Wilson <Matt@MacBook-Pro-10.lan>
2022-09-20 21:01:48 -0700

    partially fix photo gallery

--- ontour/src/pages/Artist.jsx
+++ ontour/src/pages/Artist.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import '../index.css';
-import $ from 'jquery'
+import "react-multi-carousel/lib/styles.css";
+
 function Artist() {
-
-
-
 
   return (
     <div className="artist">
@@ -21,59 +19,32 @@ function Artist() {
     <div class="carousel-inner">
       <div class="carousel-item active">
         <div class="card">
-          <img src="https://www.nationalworld.com/jpim-static/image/2022/01/07/12/GettyImages-1301817545.jpg?width=1200&enable=upscale" class="d-block w-100" alt="..."/>
+          <img src="https://www.rollingstone.com/wp-content/uploads/2022/05/jack-harlow-tour.jpg" class="d-block w-100" alt="..."/>
           <div class="card-body">
             <h5 class="card-title">Card title 2</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
       </div>
     </div>
     <div class="carousel-item">
       <div class="card">
-        <img src="https://images.complex.com/complex/images/c_fill,dpr_auto,f_auto,q_auto,w_1400/fl_lossy,pg_1/fsz3lm5oh05nslobdcol/weeknd-tour-info?fimg-ssr-default" class="d-block w-100" alt="..."/>
+        <img src="https://www.leoweekly.com/wp-content/uploads/2019/12/Harlow3.jpg" class="d-block w-100" alt="..."/>
           <div class="card-body">
             <h5 class="card-title">Card title 2</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
           </div>
       </div>
     </div>
     <div class="carousel-item">
         <div class="card">
-          <img src="https://media2.dallasobserver.com/dal/imager/u/magnum/14537619/theweeknd_mikelgalicia.jpg?cb=1660165475" class="d-block w-100" alt="..."/>
+          <img src="https://www.gannett-cdn.com/presto/2021/10/10/NA36/01000ac6-e203-4ed3-96ba-c5382420d8ac-DCQ_ACL21_SAT_010_1.JPG" class="d-block w-100" alt="..."/>
           <div class="card-body">
             <h5 class="card-title">Card title 2</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+            {/* <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
                 card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <a href="#" class="btn btn-primary">Go somewhere</a> */}
           </div>
         </div>
     </div>
-    <div class="carousel-item">
-        <div class="card">
-          <img src="https://i.pinimg.com/originals/d5/ca/4c/d5ca4c59a1541561d1566f7bf811a87b.jpg" class="d-block w-100" alt="..."/>
-          <div class="card-body">
-            <h5 class="card-title">Card title 2</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-    </div>
-    <div class="carousel-item">
-        <div class="card">
-          <img src="https://images.complex.com/complex/images/c_fill,dpr_auto,f_auto,q_auto,w_1400/fl_lossy,pg_1/vwzklxgrg5fr3hrw59yg/jack-harlow?fimg-ssr-default" class="d-block w-100" alt="..."/>
-          <div class="card-body">
-            <h5 class="card-title">Card title 2</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-    </div>
+    
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -85,6 +56,8 @@ function Artist() {
     </button>
   </div>       
 </div>
+
+
 </div>
   );
 }
