8d5b36d5edb322c87aa9be2d793dc6a581bba9ef
unknown <amchen@usc.edu>
2022-10-01 12:06:39 -0700

    udpate on stars

--- ontour/src/components/Review.jsx
+++ ontour/src/components/Review.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import '../index.css';
+import {FaStar} from "react-icons/fa"
+
 export default function Review(props)
 {
     return (
@@ -8,9 +10,28 @@ export default function Review(props)
                 <h5 class="mb-1">{props.user}</h5>
                 <small class="text-muted">{props.date}</small>
             </div>
+            <div>
             <div class="d-flex w-100 justify-content-start">
-                <p class="text-muted">{props.rating}</p>
-            </div> 
+                {[...Array(props.rating)].map(star => {
+                    return(
+                        <FaStar
+                            className="star"
+                            color = {"#ffc107"}
+                            size = {20}
+                        />   
+                    );
+                })}
+                {[...Array(5-props.rating)].map(star => {
+                    return(
+                        <FaStar
+                            className="star"
+                            color = {"#e4e5e9"}
+                            size = {20}
+                        />   
+                    );
+                })}
+            </div>
+            </div>
             <div class="d-flex w-100 justify-content-start">
                 <p class="mb-1">{props.text}</p>
             </div>
