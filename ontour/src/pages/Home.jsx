5d7ab53970675afc9aef64720d8e40a248d5d7a9
Matthew Wilson <Matt@MacBook-Pro-10.local>
2022-10-03 11:18:29 -0700

    rating system and parse forms

--- ontour/src/pages/Home.jsx
+++ ontour/src/pages/Home.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+import {useState} from 'react';
 
 function Home() {
+  const [name, setName] = useState("");
+
+  const handleSearch = event => {
+    console.log("in clicked search");
+    console.log(name);
+  }
+
+
   return (
     <div id="homepage">
       <h1 class="display-4 fw-bold" id="homeheader">OnTour</h1>
@@ -9,8 +18,8 @@ function Home() {
             <div class="col-md-8">
               <div class="search">
                 <i class="fa fa-search"></i>
-                <input type="text" class="form-control" placeholder="Search for an artist or venue"/>
-                <button class="btn btn-primary bg-dark">Search</button>
+                <input type="text" class="form-control" onChange={event => setName(event.target.value)} value={name} placeholder="Search for an artist or venue"/>
+                <button class="btn btn-primary bg-dark" onClick={handleSearch}>Search</button>
               </div>
             </div>
           </div>
