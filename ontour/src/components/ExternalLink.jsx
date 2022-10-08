06e6c555603a671a065ab9bd297710dd56d3ea42
Kwanwoo Baek <kwanwoob@usc.edu>
2022-10-08 04:06:41 -0700

    Update ExternalLink.jsx

--- ontour/src/components/ExternalLink.jsx
+++ ontour/src/components/ExternalLink.jsx
@@ -3,7 +3,7 @@ import '../index.css';
 export default function ExternalLink(props)
 {
     return (
-        <div class="col-12 col-md-2 col-sm-3 m-3 m-sm-0">
+        <div class="col-12 col-md-2 col-sm-4 m-3 m-sm-0">
             <a href={props.mediaLink} class = "social-media-icon" target="_blank" rel="noopener noreferrer">
                 <img src= {props.iconLink} alt="spotify"/>
             </a>
