145ae14a1b7fa9d90a6282a19574a83be72ed0a0
Kwanwoo Baek <kwanwoob@usc.edu>
2022-10-09 17:49:28 -0700

    add nav search functionality

--- ontour/src/ArtistNavigation.jsx
+++ ontour/src/ArtistNavigation.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import {createSearchParams, useNavigate} from "react-router-dom";
+import {useState} from "react";
 import { NavLink } from "react-router-dom";
 
-const Navigation = () => {
+function Navigation() 
+{
+  const [artist_name, setName] = useState('')
+
+  const navigate = useNavigate(); 
+  const routeChange = (artist) =>{ 
+    navigate({
+      pathname: '/artist', 
+      search: createSearchParams({
+      artist: artist_name,
+      }).toString()
+    });
+  };
+
   return (
     <nav className="navbar navbar-expand navbar-dark bg-dark">
       <div className="container">
@@ -10,8 +25,8 @@ const Navigation = () => {
           <img id="guitar" src= "images/guitar.png"/>
         </NavLink>
         <div class="navsearch">
-          <input id="input" type="text" class="form-control shadow-none" placeholder="Search for an artist or venue"/>
-          <button id="searchbutton" class="btn bg-dark">
+          <input id="input" type="text" class="form-control shadow-none" onChange={event => setName(event.target.value)} value={artist_name} placeholder="Search for an artist or venue"/>
+          <button id="searchbutton" class="btn bg-dark" onClick={routeChange}>
             <img src="images/search-icon.png" alt="..."/>
           </button>
         </div>
