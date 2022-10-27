221f96999216a4ddd89872a5f3d6c1fed34ee414
Kwanwoo Baek <kwanwoob@usc.edu>
2022-10-26 20:28:17 -0700

    Update Home.jsx

--- ontour/src/pages/Home.jsx
+++ ontour/src/pages/Home.jsx
@@ -49,7 +49,7 @@ function Home() {
                     <div class="card bg-dark">
                         <img src="https://www.syracuse.com/resizer/Gho2h8t584_ZoNDxKzM1zOIiVk4=/arc-anglerfish-arc2-prod-advancelocal/public/ZZ2V33SVNREKLF6ROTZJ32GZXI.jpeg" class="d-block w-100" alt="..."/>
                         <div class="card-body">
-                            <h5 class="card-title">Post Malone</h5>
+                            <h5 class="card-title fw-bold">Post Malone</h5>
                         </div>
                     </div>
                   </a>
@@ -59,7 +59,7 @@ function Home() {
                     <div class="card bg-dark">
                         <img src="https://media-cldnry.s-nbcnews.com/image/upload/rockcms/2022-08/jack-harlow-superbowl-citi-concert--inline2-jp-081222-08aa15.jpg" class="d-block w-100" alt="..."/>
                         <div class="card-body">
-                            <h5 class="card-title">Jack Harlow</h5>
+                            <h5 class="card-title fw-bold">Jack Harlow</h5>
                         </div>
                     </div>
                   </a>
@@ -69,7 +69,7 @@ function Home() {
                     <div class="card bg-dark">
                         <img src="https://www.99images.com/download-image/933920/1920x1280" class="d-block w-100" alt="..."/>
                         <div class="card-body">
-                            <h5 class="card-title">Elton John</h5>
+                            <h5 class="card-title fw-bold">Elton John</h5>
                         </div>
                     </div>
                   </a>
@@ -81,7 +81,7 @@ function Home() {
                     <div class="card bg-dark">
                         <img src="https://www.billboard.com/wp-content/uploads/2021/12/harry-styles-2021-billboard-1548.jpg" class="d-block w-100" alt="..."/>
                         <div class="card-body">
-                            <h5 class="card-title">Harry Styles</h5>
+                            <h5 class="card-title fw-bold">Harry Styles</h5>
                         </div>
                     </div>
                   </a>
@@ -91,7 +91,7 @@ function Home() {
                     <div class="card bg-dark">
                         <img src="https://headlineplanet.com/home/wp-content/uploads/2022/02/Dominic-Fike-on-Fallon-4.jpg" class="d-block w-100" alt="..."/>
                         <div class="card-body">
-                            <h5 class="card-title">Dominic Fike</h5>
+                            <h5 class="card-title fw-bold">Dominic Fike</h5>
                         </div>
                     </div>
                   </a>
