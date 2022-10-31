dfb9a9fccc64f9e3101f98433bbd70df377d51ba
Matthew Wilson <Matt@MacBook-Pro-10.lan>
2022-10-30 21:37:13 -0700

    fix carousel

--- ontour/src/components/Carousel.jsx
+++ ontour/src/components/Carousel.jsx
@@ -8,8 +8,14 @@ export default function Carousel(props)
 {
     const [images, setImages] = useState([]);
     const [incrementor , setIncrementor] = useState(-3);
+    const [imageLoad, setImageLoad] = useState(false);
     useEffect(() => {
         console.log(props.images);
+        console.log(images.length);
+        if(images.length)
+        {
+            setImageLoad(true);
+        }
         setImages(props.images);
     }, [props.images]);
 
@@ -40,40 +46,32 @@ export default function Carousel(props)
                     </button>
                     <div class="carousel-item active">
                         <div class="row">
-                            <CarouselImage image="https://www.leoweekly.com/wp-content/uploads/2019/12/Harlow3.jpg" text="Card title 1"/>
-                            <CarouselImage image="https://www.rollingstone.com/wp-content/uploads/2022/05/jack-harlow-tour.jpg" text="Card title 2"/>
-                            <CarouselImage image="https://www.gannett-cdn.com/presto/2021/10/10/NA36/01000ac6-e203-4ed3-96ba-c5382420d8ac-DCQ_ACL21_SAT_010_1.JPG" text="Card title 3"/>
-                            {/* {images.map((image, index) => {
+                            {imageLoad ? images.map((image, index) => {
                                 while(index < 3) {
-                                    return <CarouselImage image={image} text="Card title 1"/>
+                                    if(index <= images.length-1){
+                                        return <CarouselImage image={image} text="Card title 1"/>
+                                    }
                                 }
-                            })} */}
+                            }): console.log("howdy")}
                         </div>
                     </div>
-                    <div class="carousel-item">
-                        <div class="row">
-                            <CarouselImage image="https://s.hdnux.com/photos/01/22/47/15/21656444/4/rawImage.jpg" text="Card title 5"/>
-                            <CarouselImage image="https://media-cldnry.s-nbcnews.com/image/upload/t_fit-760w,f_auto,q_auto:best/rockcms/2022-08/jack-harlow-superbowl-citi-concert--inline2-jp-081222-08aa15.jpg" text="Card title 5"/>
-                            <CarouselImage image="https://www.okayplayer.com/wp-content/uploads/2022/06/GettyImages-1405312954.jpg" text="Card title 6"/>
-                        </div>
-                    </div>
-                    {/* {images.map((image, index) => {
-                            index+=3;
-                            setIncrementor(incrementor + 3);
-                            if(index + 3 <= images.length) {
-                                console.log(index + 3);
-                                return (
-                                    <div class="carousel-item">
-                                        <div class="row">
-                                            {incrementor <= images.length-1 ? <CarouselImage image={images[index + incrementor]} text="Card title 5"/> : null}
-                                            {incrementor+1 <= images.length-1 ? <CarouselImage image={images[index+ incrementor + 1]} text="Card title 5"/> : null}
-                                            {incrementor+2 <= images.length-1 ? <CarouselImage image={images[index+ incrementor + 2]} text="Card title 5"/> : null}
+                    { imageLoad ? images.map((image, index) => {
+                            if(index != 0 && index % 3 == 0){
+                                if(index <= images.length) {
+                                    console.log(index);
+                                    return (
+                                        <div class="carousel-item">
+                                            <div class="row">
+                                                {index <= images.length-1 ? <CarouselImage image={images[index + incrementor]} text="Card title 5"/> : null}
+                                                {index + 1 <= images.length-1 ? <CarouselImage image={images[index+ 1]} text="Card title 5"/> : null}
+                                                {index + 2 <= images.length-1 ? <CarouselImage image={images[index+ 2]} text="Card title 5"/> : null}
+                                            </div>
                                         </div>
-                                    </div>
-                                );
+                                    );
+                                }
                             }
                         }
-                    )} */}
+                    ): console.log("ciao")}
                     <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Next</span>
