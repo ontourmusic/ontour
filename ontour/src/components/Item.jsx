3925de16f38b64d3361bba0cc3bd334dcc7419fc
Kwanwoo Baek <kwanwoob@usc.edu>
2022-11-17 16:21:39 -0800

    Update Item.jsx

--- ontour/src/components/Item.jsx
+++ ontour/src/components/Item.jsx
@@ -6,7 +6,7 @@ export default function Item(props)
     return (
         <div>
             <div class="card">
-                <img src={props.image} class="d-block w-100 h-60" alt="..."/>
+                <img src={props.image} class="d-block w-100" alt="..."/>
                 <div class="card-body">
                     <h5 class="card-title">{props.text}</h5>
                 </div>
