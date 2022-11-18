5b303cd62f779ec043237384213d9ba6ea93eb2d
Kwanwoo Baek <kwanwoob@usc.edu>
2022-11-17 17:55:40 -0800

    Update Home.jsx

--- ontour/src/pages/Home.jsx
+++ ontour/src/pages/Home.jsx
@@ -28,11 +28,11 @@ function Home() {
     var artists = ["post_malone", "jack_harlow", "elton_john", "harry_styles", "dominic_fike"];
     for (var i = 0; i < 5; i++) {
       try {
-        var artistResponse = await fetch(`http://ec2-3-129-52-41.us-east-2.compute.amazonaws.com:8000/search_artist/${artists[i]}`, {mode: 'cors'});
+        var artistResponse = await fetch(`http://127.0.0.1:8000/reviews/${artists[i]}`, {mode: 'cors'});
         var artistData = await artistResponse.json();
         console.log(artistData);
         var artistId = artistData[0].artist_id;
-        var getReviews = await fetch(`http://ec2-3-129-52-41.us-east-2.compute.amazonaws.com:8000/reviews/${artistId}`, {mode: 'cors'});
+        var getReviews = await fetch(`http://127.0.0.1:8000/reviews/${artistId}`, {mode: 'cors'});
         var reviewData = await getReviews.json();
       }
       catch (error){
