03a846aa7d45aed09a87b8540ebf9c39ba31778b
Kwanwoo Baek <kwanwoob@usc.edu>
2022-11-19 16:54:41 -0800

    Mobile responsive artist banner

--- ontour/src/components/ArtistHeader.jsx
+++ ontour/src/components/ArtistHeader.jsx
@@ -2,12 +2,40 @@ import React from "react";
 import '../index.css';
 import Rating from '@mui/material/Rating';
 import StarIcon from '@mui/icons-material/Star';
+import {useState, useEffect} from "react";
 
 function ArtistHeader(props)
 {
+    const [isMobile, setIsMobile] = useState(false)
+ 
+    const handleResize = () => {
+    if (window.innerWidth <= 576) {
+        setIsMobile(true)
+    } else {
+        setIsMobile(false)
+    }
+    }
+
+    const useBeforeRender = (callback, deps) => {
+        const [isRun, setIsRun] = useState(false);
+    
+        if (!isRun) {
+            callback();
+            setIsRun(true);
+        }
+    
+        useEffect(() => () => setIsRun(false), deps);
+    };
+
+    useBeforeRender(() => handleResize(), []);
+
+    useEffect(() => {
+    window.addEventListener("resize", handleResize)
+    })
+
     return (
-        <div id="artist-background" class="container-fluid jumbotron bg-cover text-white" style={{backgroundImage: `url("${props.image}")`}}>
-            <div id="text-contain">
+        <div id="artist-background" class="container-fluid jumbotron bg-cover text-white" style={{backgroundImage: isMobile? `linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url("${props.image}")` : `linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5)), url("${props.image}")`}}>
+            <div id="text-contain" style={{backgroundColor: isMobile ? 'rgba(0, 0, 0, 0.5)' : ''}}>
                 <h1 id="artist-name" class="fw-bold">{props.name}</h1>
                 <div class="rating">
                     <Rating
