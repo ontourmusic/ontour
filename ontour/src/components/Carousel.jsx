a37813f2db132e8d5c0abc0b052ba3f9ed1ef7ec
Kwanwoo Baek <kwanwoob@usc.edu>
2022-11-20 12:49:58 -0800

    Mobile carousel

--- ontour/src/components/Carousel.jsx
+++ ontour/src/components/Carousel.jsx
@@ -44,9 +44,9 @@ export default function Carousel(props)
                 </div>
             </div>
             {/* Mobile Carousel */}
-            <div id="carouselExampleControls" class="carousel slide d-block d-sm-none" data-bs-ride="carousel">
+            <div id="mobileCarousel" class="carousel slide d-block d-sm-none" data-bs-ride="carousel">
                 <div class="carousel-inner">
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <button class="carousel-control-prev" type="button" data-bs-target="#mobileCarousel" data-bs-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Previous</span>
                     </button>
@@ -66,7 +66,7 @@ export default function Carousel(props)
                             )
                         }
                     }): <div></div>}
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <button class="carousel-control-next" type="button" data-bs-target="#mobileCarousel" data-bs-slide="next">
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Next</span>
                     </button>
