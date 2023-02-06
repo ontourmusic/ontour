1bdb1102c9aad77c3c60bf41302aa7e6d1a93d62
Tristan Whang <twhang@usc.edu>
2023-02-06 00:02:14 -0800

    slightly modified the polaroid background color but not sold on it

--- ontour/src/components/Polaroid.jsx
+++ ontour/src/components/Polaroid.jsx
@@ -11,9 +11,9 @@ expected props:
 */
 const Polaroid = (props) => {
     return (
-        <div class="col-4" onClick={props.onPress}>
+        <div class="col-4" onClick={props.onPress} >
             <a href={props.link} >
-                <div class="card bg-light">
+                <div class="card" style={styles.polaroid_background}>
                     <img src={props.imageURL} class="d-block w-100" style={styles.polaroid_image} alt="..." />
                     <div class="card-body">
                         {props.bottomComponent}
