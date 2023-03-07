2e37e2e05b4f64302160303dd8fde01d1ee292dd
Matthew Wilson <mtwilson@usc.edu>
2023-03-06 21:45:31 -0800

    start fixing backend

--- api/main.py
+++ api/main.py
@@ -16,6 +16,8 @@ from models import Reviews as ModelReviews
 
 from models import Venue_Reviews as ModelVenue_Reviews
 
+from supabase import create_client, Client
+
 import os
 from dotenv import load_dotenv
 
@@ -58,6 +60,11 @@ app.add_middleware(
 # to avoid csrftokenError
 app.add_middleware(DBSessionMiddleware, db_url='postgresql://postgres:ontour@3.129.52.41/postgres')
 
+url = 'https://zouczoaamusrlkkuoppu.supabase.co'
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvdWN6b2FhbXVzcmxra3VvcHB1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY3ODE1ODUyMSwiZXhwIjoxOTkzNzM0NTIxfQ.LTuL_u0tzmsj8Zf9m6JXN4JivwLq1aRXvU2YN-nDLCo"
+supabase: Client = create_client(url, key)
+
+
 @app.get("/")
 async def root():
     return {"message": "hello world"}
@@ -87,65 +94,57 @@ async def author(author:SchemaArtist, fname: str, lname: str):
 
 @app.get('/artist/')
 async def author():
-    artist = db.session.query(ModelArtist).all()
+    artist = supabase.table('artists').select('*').execute()
     return artist
 
 @app.get('/artist/{artist_id}')
 async def author(artist_id: int):
-    print("HI")
-    artist = db.session.query(ModelArtist).filter(ModelArtist.artist_id == artist_id).first()
+    artist = supabase.table('artists').select('*').eq('artist_id', artist_id).execute()
     return artist
 
 @app.get('/reviews/{artist_id}')
 async def reviews(artist_id: int):
-    reviews = db.session.query(ModelReviews).filter(ModelReviews.artist_id == artist_id).all()
+    reviews = supabase.table('artist_reviews').select('*').eq('artist_id', artist_id).execute()
     return reviews
 
 @app.post('/reviews/')
-async def reviews(artist_id: int, event_id: int, rating: float, description: str, fname: str, lname: str, eventname: str, date: str):
-    db_review = ModelReviews(artist_id= artist_id, event_id= event_id, rating=rating, description=description, fname=fname, lname=lname, eventname=eventname, date=date)
-    db.session.add(db_review)
-    db.session.commit()
+async def reviews(artist_id: int, venue: str, rating: float, description: str, fname: str, lname: str, eventname: str, date: str):
+    db_review = supabase.table('artist_reviews').insert({'artist_id': artist_id,'rating': rating, 'review': description, 'fname': fname, 'lname': lname, 'event': eventname, 'eventDate': date}).execute()
     return db_review
 
-@app.get('/search_artist/{search_text}')
-async def search_artist(search_text: str):
-    print(search_text)
-    conn = psycopg2.connect(user="postgres", password="ontour", host="ec2-3-129-52-41.us-east-2.compute.amazonaws.com", port="5432", database="postgres")
-    cur = conn.cursor()
-    print("Database opened successfully")
-    print(conn.get_dsn_parameters(), "\n")
-    print("SELECT * FROM search_artist_text(" + search_text + ");")
-    cur.execute("SELECT * FROM search_artist_text('" + search_text + "');")
-    rows = cur.fetchall()
-    res_dict = []
-    headers = ["artist_id", "fname", "lname", "genre", "alias", "image_url", "images"]
-    for row in rows:
-        print(row)
-        res_row = dict(zip(headers, row))
-        res_dict.append(res_row)
-    
-    #jsonify(rows)
-    res = dict(zip(headers, rows[0]))
-    # res_json = json.dumps(res, indent = 4) 
+# @app.get('/search_artist/{search_text}')
+# async def search_artist(search_text: str):
+#     print(search_text)
+#     conn = psycopg2.connect(user="postgres", password="ontour", host="ec2-3-129-52-41.us-east-2.compute.amazonaws.com", port="5432", database="postgres")
+#     cur = conn.cursor()
+#     print("Database opened successfully")
+#     print(conn.get_dsn_parameters(), "\n")
+#     print("SELECT * FROM search_artist_text(" + search_text + ");")
+#     cur.execute("SELECT * FROM search_artist_text('" + search_text + "');")
+#     rows = cur.fetchall()
+#     res_dict = []
+#     headers = ["artist_id", "fname", "lname", "genre", "alias", "image_url", "images"]
+#     for row in rows:
+#         print(row)
+#         res_row = dict(zip(headers, row))
+#         res_dict.append(res_row)
+#     res = dict(zip(headers, rows[0]))
+#     return res_dict
 
+# @app.get('/reviews/')
+# async def reviews():
+#     conn = psycopg2.connect(user="postgres", password="ontour", host="ec2-3-129-52-41.us-east-2.compute.amazonaws.com", port="5432", database="postgres")
+#     cur = conn.cursor()
+#     cur.execute("SELECT artist_id, rating FROM reviews;")
+#     rows = cur.fetchall()
+#     res_dict = []
+#     headers = ["artist_id", "rating"]
+#     for row in rows:
+#         print(row)
+#         res_row = dict(zip(headers, row))
+#         res_dict.append(res_row)
 
-    return res_dict
-
-@app.get('/reviews/')
-async def reviews():
-    conn = psycopg2.connect(user="postgres", password="ontour", host="ec2-3-129-52-41.us-east-2.compute.amazonaws.com", port="5432", database="postgres")
-    cur = conn.cursor()
-    cur.execute("SELECT artist_id, rating FROM reviews;")
-    rows = cur.fetchall()
-    res_dict = []
-    headers = ["artist_id", "rating"]
-    for row in rows:
-        print(row)
-        res_row = dict(zip(headers, row))
-        res_dict.append(res_row)
-
-    return res_dict
+#     return res_dict
 
 @app.get('/search_venue/{search_text}')
 async def search_venue(search_text: str):
@@ -166,14 +165,12 @@ async def search_venue(search_text: str):
 
 @app.get('/venue_reviews/{venue_id}')
 async def reviews(venue_id: int):
-    reviews = db.session.query(ModelVenue_Reviews).filter(ModelVenue_Reviews.venue_id == venue_id).all()
-    return reviews
+    venue = supabase.table('venue_reviews').select('*').eq('venue_id', venue_id).execute()
+    return venue
 
 @app.post('/venue_reviews/')
 async def reviews(venue_id: int, rating: float, description: str, name: str, artistname: str, date: str):
-    db_review = ModelVenue_Reviews(venue_id= venue_id, rating=rating, description=description, name=name, artistname=artistname, date=date)
-    db.session.add(db_review)
-    db.session.commit()
+    db_review = supabase.table('venue_reviews').insert({'venue_id': venue_id,'rating': rating, 'review': description, 'name': name, 'artist': artistname, 'eventDate': date}).execute()
     return db_review
 
 
