91e61de3b86f5698d731c68edb2fc9dee6982628
Tristan Whang <twhang@usc.edu>
2023-03-20 21:37:56 -0700

    Made the category section but iut doesnt link anywhere for now

---
+++ ontour/src/Styles/category_styles.js
@@ -0,0 +1,28 @@
+
+
+const container = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: "200px",
+    // padding: "100px auto 100px auto",
+    border: "1px solid white",
+    backgroundColor: "gray",
+    opacity: 0.85,
+};
+const icon = {
+    width: 50,
+    height: 50,
+    paddingBottom: 5
+};
+const content = {
+};
+
+const category_styles = {
+    container,
+    icon,
+    content,
+};
+
+export default category_styles;
