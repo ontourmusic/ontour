c4901d8f9c6e700b47c700be813d01c648b4ebda
Noah Hauptmann <nhauptma@usc.edu>
2023-04-05 22:53:15 -0700

    Login

--- ontour/src/Navigation.jsx
+++ ontour/src/Navigation.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Navigation = () => {
+  const { user, isAuthenticated } = useAuth0();
+
   return (
     <nav className="navbar navbar-expand navbar-dark bg-dark">
       <div className="container">
         <a href="/">
           <img id="nav-logo" src= "images/tourScoutBack.png" alt=""/>
         </a>
-        <LoginButton></LoginButton>
-        <LogoutButton></LogoutButton>
+        {!isAuthenticated && <LoginButton></LoginButton>}
+        {isAuthenticated && <LogoutButton></LogoutButton>}
       </div>
     </nav>
   );
--- ontour/src/components/LoginButton.jsx
+++ ontour/src/components/LoginButton.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import button_styles from "../Styles/button_styles"
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return <button style={button_styles.loginButton} onClick={() => loginWithRedirect()}>Log In</button>;
 };
 
 export default LoginButton;
