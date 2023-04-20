ada0bf1d178602e3d167f9a10a6821ca3a1eee18
Tristan Whang <twhang@usc.edu>
2023-04-20 11:57:39 -0700

    changed image polaroids to be more like slides. I think it looks much better

--- ontour/src/Styles/polaroid_styles.js
+++ ontour/src/Styles/polaroid_styles.js
@@ -2,35 +2,20 @@ import OnTourColors from "./colors";
 import common_styles from "./common_styles";
 
 const polaroid_image = {
-  width: "fit-content",
-  padding: "5px",
-  // width: "100%",
-  height: "275px",
+  height: "100%",
   objectFit: "cover",
-  borderRadius: common_styles.borderRadius.default,
   cursor: "pointer",
 };
-const polaroid_image_inner = {
-  width: "100%",
-  height: "275px",
-  objectFit: "cover",
-};
-
-const polaroid_background = {
-  backgroundColor: OnTourColors.colors.surface,
-  borderRadius: common_styles.borderRadius.default,
-};
 const polaroid_container = {
-  hight: "100%",
-  padding: "5px",
+  marginTop: "7px",
+  marginBottom: "7px",
+  height: "30vh",
+  boxShadow: "0px 0px 7px -1px rgba(0,0,0,0.56)",
 };
 
 const polaroid_styles = {
   polaroid_image,
-  polaroid_background,
-  polaroid_image_inner,
   polaroid_container,
-
 };
 
 export default polaroid_styles;
