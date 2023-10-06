3247cc1f87c3318cbf38163922be4432fd861fe2
rachelbakke <rachelbakke10@gmail.com>
2023-10-06 11:59:54 -0700

    SideContent with Links

--- ontour/src/pages/AccountSettings.jsx
+++ ontour/src/pages/AccountSettings.jsx
@@ -5,7 +5,8 @@ import { Helmet } from "react-helmet";
 import { Grid } from "@mui/material";
 
 import Navigation from "../Navigation";
-import BusinessSidebar from "../components/BusinessSidebar"
+import BusinessSidebar from "../components/BusinessSidebar";
+import SideContent from "../components/SideContent";
 import Footer from "../components/Footer";
 
 
@@ -24,7 +25,7 @@ function AccountSettings() {
                     <BusinessSidebar />
                 </Grid>
                 <Grid item xs={12} md={8}>
-                    
+                    <SideContent name={"Links"} linkPairs={[["www.https://open.spotify.com/artist/06HL4z0CvFAxyc27GXpf02", "images/spotify_icon.png"], ["https://www.instagram.com/taylorswift/?hl=en", "images/instagram.png.webp"], ["https://twitter.com/taylorswift13?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor", "images/twitter.png"]]}/>
                 </Grid>
                 <Grid item xs={12}>
                     <hr id="artist-footer"></hr>
