e58f5dbc96fc25b35d30b9bc5161a70152a8cbac
rachelbakke <rachelbakke10@gmail.com>
2023-10-06 10:44:04 -0700

    Removed Artist Components from HTML part

--- ontour/src/pages/AccountSettings.jsx
+++ ontour/src/pages/AccountSettings.jsx
@@ -24,11 +24,14 @@ import ArtistContent from "../components/ArtistContent";
 import ImageCarousel from "../components/ImageCarousel"; 
 
 function AccountSettings() {
+    //functions 
+
+
 
     return (
         <>
             <Helmet>
-                <title>{fullName}</title>
+                <title>Account Settings</title>
                 <script async src='https://www.googletagmanager.com/gtag/js?id=G-BE8WDNBGS7'></script>
                 <script>
                     {`
@@ -50,32 +53,7 @@ function AccountSettings() {
                 </script>
             </Helmet>
             <Grid container spacing={0}>
-                <Grid item xs={12}>
-                    <ArtistNavigation />
-                </Grid>
-                <Grid item xs={12}>
-                    <ArtistHeader images={imageArray} name={fullName} rating={aggregateRating} total={totalReviews} image={artistImage} isVenue={0} onTour={onTour} verified={false}/>
-                </Grid>
                 <Grid container spacing={1} style={artist_styles.grid.body_container}>
-                    <Grid item xs={12} md={8}>
-                        <ImageCarousel artistID={artistID} images={imageArray} 
-                            slideCount={window.innerWidth < common_styles.window_breakpoints.sm ? 1 : 3} />
-                        <ArtistContent 
-                        allReviews={allReviews} 
-                        filteredReviews={filteredReviews} 
-                        aggregateRating={aggregateRating} 
-                        onFormChange={formChange} 
-                        onRatingChange={ratingFilter} 
-                        onReviewSearch={searchReviews} 
-                        searchResults={showResults} 
-                        onClearSearch={clearSearch} 
-                        reviewTable={"artist_reviews"}
-                            />
-                        {fullName !== "" && <WriteReview artistId={artistIdNumber} name={fullName} numReviews={totalReviews}/>}
-                    </Grid>
-                    <Grid item xs={12} md={4}>
-                        <SideContent name={fullName} linkPairs={[[spotifyLink, "images/spotify_icon.png"], [instaLink, "images/instagram.png.webp"], [twitterLink, "images/twitter.png"]]} />
-                    </Grid>
                 </Grid>
                 <Grid item xs={12}>
                     <hr id="artist-footer"></hr>
