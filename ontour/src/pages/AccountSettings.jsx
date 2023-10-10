dc84d9f8dc1287d29593b052d5df3e542f969eae
rachelbakke <rachelbakke10@gmail.com>
2023-10-10 12:07:11 -0700

    Single Textfield, not editable or preload with data

--- ontour/src/pages/AccountSettings.jsx
+++ ontour/src/pages/AccountSettings.jsx
@@ -1,17 +1,23 @@
-import React from "react";
+//import React from "react";
+import React, { useState } from 'react';
 import "../index.css";
 import "react-multi-carousel/lib/styles.css"; 
 import { Helmet } from "react-helmet";
-import { Grid } from "@mui/material";
+import { Grid, TextField } from "@mui/material";
 
 import Navigation from "../Navigation";
 import BusinessSidebar from "../components/BusinessSidebar";
 import SideContent from "../components/SideContent";
 import Footer from "../components/Footer";
+import ExternalLink from "../components/ExternalLink";
+import EditableTextBox from '../components/EditableTextBox';
 
 
 function AccountSettings() {
     //functions 
+    let linkPairs = [["www.https://open.spotify.com/artist/06HL4z0CvFAxyc27GXpf02", "images/spotify_icon.png"], ["https://www.instagram.com/taylorswift/?hl=en", "images/instagram.png.webp"], ["https://twitter.com/taylorswift13?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor", "images/twitter.png"]];
+    const [textLink1, setTextLink1] = useState('');  // Initial text is empty 
+    //textLink1 = "https://www.instagram.com/taylorswift/?hl=en";
 
     return (
         <>
@@ -25,7 +31,20 @@ function AccountSettings() {
                     <BusinessSidebar />
                 </Grid>
                 <Grid item xs={12} md={8}>
-                    <SideContent name={"Links"} linkPairs={[["www.https://open.spotify.com/artist/06HL4z0CvFAxyc27GXpf02", "images/spotify_icon.png"], ["https://www.instagram.com/taylorswift/?hl=en", "images/instagram.png.webp"], ["https://twitter.com/taylorswift13?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor", "images/twitter.png"]]}/>
+                {linkPairs && 
+                <div>
+                    {
+                        linkPairs.map((pair) => {
+                            return <ExternalLink mediaLink={pair[0]} iconLink={pair[1]} />
+                        })
+                    }
+                        </div>}
+                    <TextField
+                    value={textLink1} //draw from database
+                    label="Enter your link"
+                    onChange={(e) => { 
+                        setTextLink1(e.target.value); 
+                    }} />
                 </Grid>
                 <Grid item xs={12}>
                     <hr id="artist-footer"></hr>
