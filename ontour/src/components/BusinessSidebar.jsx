2abcb1b344833bda4560961baaaf735d701d821b
Nicholas-Chu <nick.chu75@gmail.com>
2023-10-10 11:27:32 -0700

    finalized businesssidebar

--- ontour/src/components/BusinessSidebar.jsx
+++ ontour/src/components/BusinessSidebar.jsx
@@ -5,12 +5,12 @@ import { useHistory } from 'react-router-dom';
 
 const BusinessSidebar = () => {
     return (
-        <Box display="grid" gap={2} bgcolor="grey.200" p={2} borderRadius={2} boxShadow={3}>
-        <h1>My Dashboard</h1>
-        <Button color="primary" fullWidth component={Link} to="/fan-analytics">Fan Analytics</Button>
-        <Button color="primary" fullWidth component={Link} to="/reviews">Reviews</Button>
-        <Button color="primary" fullWidth component={Link} to="/tours">Tours</Button>
-        <Button color="primary" fullWidth component={Link} to="/account-settings">Account Settings</Button>
+        <Box display="grid" gap={2} bgcolor="grey.200" p={2} boxShadow={3}>
+            <h3>My Dashboard</h3>
+            <Button color="primary" fullWidth component={Link} to="/fan-analytics" sx={{color: 'black'}}>Fan Analytics</Button>
+            <Button color="primary" fullWidth component={Link} to="/reviews" sx={{color: 'black'}}>Reviews</Button>
+            <Button color="primary" fullWidth component={Link} to="/tours" sx={{color: 'black'}}>Tours</Button>
+            <Button color="primary" fullWidth component={Link} to="/account" sx={{color: 'black'}}>Account Settings</Button>
         </Box>
       );
 
