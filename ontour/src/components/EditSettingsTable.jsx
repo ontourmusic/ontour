98dcac833a8ca355cd95345b9a08dd607288ff84
Nicholas-Chu <nick.chu75@gmail.com>
2023-10-23 17:45:25 -0700

    edit settings username

--- ontour/src/components/EditSettingsTable.jsx
+++ ontour/src/components/EditSettingsTable.jsx
@@ -1,29 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import { Grid, TextField, Button } from '@mui/material';
-
-// const EditSettingsTable = () => {
-//   return (
-//     <Grid container spacing={4} direction="column" sx={{padding:'20px'}}>
-//       <Grid item xs={12}>
-//         <TextField fullWidth label="Email" variant="outlined" defaultValue="taylor.swift@gmail.com" />
-//       </Grid>
-
-//       <Grid item xs={12}>
-//         <TextField fullWidth label="Password" type="password" variant="outlined" />
-//       </Grid>
-
-//       <Grid item xs={12}>
-//         <TextField fullWidth label="Official Profile Name" variant="outlined" />
-//         <Button variant="text">Request Edit Access</Button>
-//       </Grid>
-//       <Grid item xs={12}>
-//         <Button variant="contained" color="primary"> Save Changes</Button>
-//       </Grid>
-//     </Grid>
-//   );
-// }
+import { useAuth0 } from "@auth0/auth0-react";
 
 const EditSettingsTable = () => {
+    const { isAuthenticated, user } = useAuth0();
+   // const initialEmail = isAuthenticated ? user.email : '';
+    const [userEmail, setUserEmail] = useState("");
+
+    useEffect(() => {
+      if (isAuthenticated && user && user.email) {
+          setUserEmail(user.email);
+      }
+    }, [user, isAuthenticated]);
+
     return (
       <Grid container spacing={4} direction="column" sx={{ padding: '20px' }}>
         
@@ -31,9 +20,12 @@ const EditSettingsTable = () => {
           <Grid item xs={9}>
             <TextField 
               fullWidth
-              label="Email" 
+              label="Email"
               variant="outlined" 
-              defaultValue="taylor.swift@gmail.com" 
+              value={userEmail}
+              onChange={(e) => { 
+                setUserEmail(e.target.value); 
+              }}
             />
           </Grid>
         </Grid>
