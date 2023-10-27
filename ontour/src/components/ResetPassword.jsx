06674405919a71ec9ea1c13ad899abea937ca8d8
Owen Hsieh <ohsieh@usc.edu>
2023-10-27 10:51:19 -0700

    get user tests

--- ontour/src/components/ResetPassword.jsx
+++ ontour/src/components/ResetPassword.jsx
@@ -1,33 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
 import {Button} from '@mui/material';
 import button_styles from "../Styles/button_styles"
 
 const ResetPassword = () => {
     // const { loginWithRedirect } = useAuth0();
-    const { user, getAccessTokenSilently } = useAuth0();
-    
-    // const [email, setEmail] = useState('');
+    const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+    const [userMetadata, setUserMetadata] = useState(null);
+
+    const [email, setEmail] = useState('');
     const apiURL = "https://dev-uujtiin6xxo47cy3.us.auth0.com";
     const handleResetPassword = async () => {
         try {
             // const userid = user.sub.split('|')[1];
-            console.log('metadataResponse');
-            const accessToken = await getAccessTokenSilently({
-                authorizationParams: {
-                  audience: `https://dev-uujtiin6xxo47cy3.us.auth0.com/api/v2/`,
-                  scope: "read:current_user",
-                },
-              });
-              console.log('metadataResponse');
-              const userDetailsByIdUrl = `https://dev-uujtiin6xxo47cy3.us.auth0.com/api/v2/users/${user.sub}`;
+            // console.log('metadataResponse');
+            // const accessToken = await getAccessTokenSilently({
+            //     authorizationParams: {
+            //       audience: `https://dev-uujtiin6xxo47cy3.us.auth0.com/api/v2/`,
+            //       scope: "read:current_user",
+            //     },
+            //   });
+            //   console.log('metadataResponse');
+            //   const userDetailsByIdUrl = `https://dev-uujtiin6xxo47cy3.us.auth0.com/api/v2/users/${user.sub}`;
 
-              const metadataResponse = await fetch(userDetailsByIdUrl, {
-                headers: {
-                  Authorization: `Bearer ${accessToken}`,
-                },
-              });
-              console.log(metadataResponse);
+            //   const metadataResponse = await fetch(userDetailsByIdUrl, {
+            //     headers: {
+            //       Authorization: `Bearer ${accessToken}`,
+            //     },
+            //   });
+            //   console.log(metadataResponse);
             // console.log(user);
             // console.log('token ' + accessToken);
             // const b = {
@@ -35,31 +36,31 @@ const ResetPassword = () => {
             // }
 
 
-            const clientID = 'uKg34leiFuxWAdA7Fc9uMI7vbqahNLIW';
-            const clientSecret = 'yOt1486_enjorpvpBeK8NqYTNyjHYnXK2T7W63ac1IL_BizZmKV1lRvUm03ARM5v';
-            const dataopt = {
-                grant_type: 'client_credentials',
-                client_id: clientID,
-                client_secret: clientSecret,
-                audience: apiURL + 'api/v2/'
-              }
+            // const clientID = 'uKg34leiFuxWAdA7Fc9uMI7vbqahNLIW';
+            // const clientSecret = 'yOt1486_enjorpvpBeK8NqYTNyjHYnXK2T7W63ac1IL_BizZmKV1lRvUm03ARM5v';
+            // const dataopt = {
+            //     grant_type: 'client_credentials',
+            //     client_id: clientID,
+            //     client_secret: clientSecret,
+            //     audience: apiURL + 'api/v2/'
+            //   }
 
-            const options = {
-                method: 'POST',
-                headers: {'content-type': 'application/json'},
-                body: JSON.stringify({
-                    client_id: 'uKg34leiFuxWAdA7Fc9uMI7vbqahNLIW',
-                    client_secret: 'yOt1486_enjorpvpBeK8NqYTNyjHYnXK2T7W63ac1IL_BizZmKV1lRvUm03ARM5v',
-                    audience: 'https://dev-uujtiin6xxo47cy3.us.auth0.com/api/v2/',
-                    grant_type: 'client_credentials',
-                  }),
+            // const options = {
+            //     method: 'POST',
+            //     headers: {'content-type': 'application/json'},
+            //     body: JSON.stringify({
+            //         client_id: 'uKg34leiFuxWAdA7Fc9uMI7vbqahNLIW',
+            //         client_secret: 'yOt1486_enjorpvpBeK8NqYTNyjHYnXK2T7W63ac1IL_BizZmKV1lRvUm03ARM5v',
+            //         audience: 'https://dev-uujtiin6xxo47cy3.us.auth0.com/api/v2/',
+            //         grant_type: 'client_credentials',
+            //       }),
         
-            };
-            const req = await fetch('https://dev-uujtiin6xxo47cy3.us.auth0.com/oauth/token', options);
-            console.log(req);
+            // };
+            // const req = await fetch('https://dev-uujtiin6xxo47cy3.us.auth0.com/oauth/token', options);
+            // console.log(req);
             
 
-            const tok = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkxVUUJ1U0liRDB3WmIzNWUxNXFpaSJ9.eyJpc3MiOiJodHRwczovL2Rldi11dWp0aWluNnh4bzQ3Y3kzLnVzLmF1dGgwLmNvbS8iLCJzdWIiOiJ1S2czNGxlaUZ1eFdBZEE3RmM5dU1JN3ZicWFoTkxJV0BjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9kZXYtdXVqdGlpbjZ4eG80N2N5My51cy5hdXRoMC5jb20vYXBpL3YyLyIsImlhdCI6MTY5ODM4OTAzNywiZXhwIjoxNjk4NDc1NDM3LCJhenAiOiJ1S2czNGxlaUZ1eFdBZEE3RmM5dU1JN3ZicWFoTkxJVyIsInNjb3BlIjoicmVhZDp1c2VycyB1cGRhdGU6dXNlcnMgZGVsZXRlOnVzZXJzIGNyZWF0ZTp1c2VycyByZWFkOnVzZXJzX2FwcF9tZXRhZGF0YSB1cGRhdGU6dXNlcnNfYXBwX21ldGFkYXRhIGRlbGV0ZTp1c2Vyc19hcHBfbWV0YWRhdGEgY3JlYXRlOnVzZXJzX2FwcF9tZXRhZGF0YSIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.ZEfB_ARWov0PGBnXsVS6ZC4IZR4gxmNc8jc1JMEe8whT00O3GMCL_s0XFzW5m0b-wR3tcAOHWrpiYWqF4vXoRjZ_wdBUMoNB4SuUiCZPksgOoFsZhg1cLvuttgD9mpWj5wBHoC-INNQyD4aRFE9sE_LCx07w_9ArfZSp60DpsgFAkmyr7A0YztDjS77oW7pviiTygL0tfvveknZzuIU90TFQ4omDLhjsgJ6xcsA0VcwWY8vVRTC1VE7-bMWwRKsaOuYijIu5Fx70pWV6hIvE2rF-0s-hOJe6TRVzrPhS9GnpKLTDYMkQ4aaBx3PFAY3JsyTBZXokKBQf8CBkwzlDhg';
+            const tok = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkxVUUJ1U0liRDB3WmIzNWUxNXFpaSJ9.eyJpc3MiOiJodHRwczovL2Rldi11dWp0aWluNnh4bzQ3Y3kzLnVzLmF1dGgwLmNvbS8iLCJzdWIiOiJ1S2czNGxlaUZ1eFdBZEE3RmM5dU1JN3ZicWFoTkxJV0BjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9kZXYtdXVqdGlpbjZ4eG80N2N5My51cy5hdXRoMC5jb20vYXBpL3YyLyIsImlhdCI6MTY5ODM4OTAzNywiZXhwIjoxNjk4NDc1NDM3LCJhenAiOiJ1S2czNGxlaUZ1eFdBZEE3RmM5dU1JN3ZicWFoTkxJVyIsInNjb3BlIjoicmVhZDp1c2VycyB1cGRhdGU6dXNlcnMgZGVsZXRlOnVzZXJzIGNyZWF0ZTp1c2VycyByZWFkOnVzZXJzX2FwcF9tZXRhZGF0YSB1cGRhdGU6dXNlcnNfYXBwX21ldGFkYXRhIGRlbGV0ZTp1c2Vyc19hcHBfbWV0YWRhdGEgY3JlYXRlOnVzZXJzX2FwcF9tZXRhZGF0YSIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.ZEfB_ARWov0PGBnXsVS6ZC4IZR4gxmNc8jc1JMEe8whT00O3GMCL_s0XFzW5m0b-wR3tcAOHWrpiYWqF4vXoRjZ_wdBUMoNB4SuUiCZPksgOoFsZhg1cLvuttgD9mpWj5wBHoC-INNQyD4aRFE9sE_LCx07w_9ArfZSp60DpsgFAkmyr7A0YztDjS77oW7pviiTygL0tfvveknZzuIU90TFQ4omDLhjsgJ6xcsA0VcwWY8vVRTC1VE7-bMWwRKsaOuYijIu5Fx70pWV6hIvE2rF-0s-hOJe6TRVzrPhS9GnpKLTDYMkQ4aaBx3PFAY3JsyTBZXokKBQf8CBkwzlDhg'
             // console.log(req);
             const opt = {
                 method: 'GET',
@@ -67,11 +68,11 @@ const ResetPassword = () => {
                     authorization: 'Bearer ' + tok
                 },
             }
-            const asdf = 'https://dev-uujtiin6xxo47cy3.us.auth0.com/api/v2/users/auth0|65259701f4f99b2bb4cfe3d7';
-            console.log(asdf);
+            const getAPI = 'https://dev-uujtiin6xxo47cy3.us.auth0.com/api/v2/users/' + user.sub;
+            console.log(getAPI);
             console.log(opt);
-            const userInfo = await fetch(asdf, opt);
-            console.log('userInfo');
+            const userInfo = await fetch(getAPI, opt);
+            // console.log('userInfo');
             console.log(userInfo);
 
             // console.log(userInfo);
@@ -115,6 +116,55 @@ const ResetPassword = () => {
             {/* <button onClick={handleResetPassword}>Reset Password</button> */}
         </div>
     );
+    
+
+    // useEffect(() => {
+    //     const getUserMetadata = async () => {
+    //       const domain = "dev-uujtiin6xxo47cy3.us.auth0.com";
+      
+    //       try {
+    //         const accessToken = await getAccessTokenSilently({
+    //           authorizationParams: {
+    //             audience: `https://${domain}/api/v2/`,
+    //             scope: "read:current_user",
+    //           },
+    //         });
+      
+    //         const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
+      
+    //         const metadataResponse = await fetch(userDetailsByIdUrl, {
+    //           headers: {
+    //             Authorization: `Bearer ${accessToken}`,
+    //           },
+    //         });
+      
+    //         const { user_metadata } = await metadataResponse.json();
+      
+    //         setUserMetadata(user_metadata);
+    //       } catch (e) {
+    //         console.log(e.message);
+    //       }
+    //     };
+      
+    //     getUserMetadata();
+    //   }, [getAccessTokenSilently, user?.sub]);
+
+    // return (
+    //     isAuthenticated && (
+    //       <div>
+    //         <img src={user.picture} alt={user.name} />
+    //         <h2>{user.name}</h2>
+    //         <p>{user.email}</p>
+    //         <h3>User Metadata</h3>
+    //         {userMetadata ? (
+    //           <pre>{JSON.stringify(userMetadata, null, 2)}</pre>
+    //         ) : (
+    //           "No user metadata defined"
+    //         )}
+    //       </div>
+    //     )
+    //   );
+    
 };
 
 export default ResetPassword;
