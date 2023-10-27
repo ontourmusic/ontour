18bc936800593c70b39dd4662ad42e56d70a9c83
rachelbakke <rachelbakke10@gmail.com>
2023-10-27 11:38:22 -0700

    Create FanAnalytics Page

---
+++ ontour/src/pages/FanAnalytics.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import "../index.css";
+import "react-multi-carousel/lib/styles.css"; 
+import { Helmet } from "react-helmet";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Grid, TextField, Button} from "@mui/material";
+
+import Navigation from "../Navigation";
+import BusinessSidebar from "../components/BusinessSidebar";
+import SideContent from "../components/SideContent";
+import Footer from "../components/Footer";
+import ExternalLink from "../components/ExternalLink";
+//import EditableTextBox from '../components/EditableTextBox';
+import ResetPassword from '../components/ResetPassword';
+import { createClient } from '@supabase/supabase-js';
+import EditSettingsTable from '../components/EditSettingsTable';
+
+
+function AccountSettings() {
+  
+    const { isAuthenticated, user } = useAuth0();
+    const [userEmail, setUserEmail] = useState("");
+
+    useEffect(() => {
+      if (isAuthenticated && user && user.email) {
+          setUserEmail(user.email);
+      }
+    }, [user, isAuthenticated]);
+
+
+
+    const supabase = createClient('https://zouczoaamusrlkkuoppu.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvdWN6b2FhbXVzcmxra3VvcHB1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY3ODE1ODUyMSwiZXhwIjoxOTkzNzM0NTIxfQ.LTuL_u0tzmsj8Zf9m6JXN4JivwLq1aRXvU2YN-nDLCo');
+    
+
+
+    useEffect(() => {
+
+    }, []);
+
+    return (
+        <>
+            <Helmet>
+            </Helmet>
+            <Grid container spacing={1}>
+                <Grid item xs={12}>
+                    <Navigation navbar={false}/>
+                </Grid>
+                <Grid item xs={12} md={3}>
+                    <BusinessSidebar />
+                </Grid>
+                <Grid item xs={12}>
+                    <hr id="artist-footer"></hr>
+                    <Footer />
+                </Grid>
+            </Grid >
+        </>
+    );
+}
+export default AccountSettings;
