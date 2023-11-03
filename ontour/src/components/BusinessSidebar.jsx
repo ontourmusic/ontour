45263a1c7b5e7ae9c3da2059882caf20ee56160d
rachelbakke <rachelbakke10@gmail.com>
2023-11-03 10:46:29 -0700

    Added Under Construction for sidebar functionality

--- ontour/src/components/BusinessSidebar.jsx
+++ ontour/src/components/BusinessSidebar.jsx
@@ -8,7 +8,7 @@ const BusinessSidebar = () => {
         <Box display="grid" gap={2} bgcolor="grey.200" p={2} boxShadow={3}>
             <h3>My Dashboard</h3>
             <Button color="primary" fullWidth component={Link} to="/fan-analytics" sx={{color: 'black'}}>Fan Analytics</Button>
-            <Button color="primary" fullWidth component={Link} to="/reviews" sx={{color: 'black'}}>Reviews</Button>
+            <Button color="primary" fullWidth component={Link} to="/manage-reviews" sx={{color: 'black'}}>Reviews</Button>
             <Button color="primary" fullWidth component={Link} to="/tours" sx={{color: 'black'}}>Tours</Button>
             <Button color="primary" fullWidth component={Link} to="/account" sx={{color: 'black'}}>Account Settings</Button>
         </Box>
