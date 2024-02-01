8f54ae289102ff1d26b739bb4fdf55ccaed3861f
Shixiang Yang <adam_yang2021@163.com>
2024-02-01 10:29:46 -0800

    fix/fix hometile click navigate issue

--- ontour/src/Styles/header_styles.js
+++ ontour/src/Styles/header_styles.js
@@ -1,61 +1,60 @@
-
 const button_position = {
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "flex-end",
+  height: "100%",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "flex-end",
 };
 
 const button = {
-    // margin: "20px",
-    marginBottom: "2rem",
-    marginRight: "20px",
-    marginLeft: "20px",
-    // marginBottom: "2rem",
-    // marginRight: "2rem",
-    // marginLeft: "2rem",
-    height: '50px',
-    textTransform: 'none', 
-    fontFamily: "Helvetica", 
-    fontWeight:'bold',
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-}
+  // margin: "20px",
+  marginBottom: "2rem",
+  marginRight: "20px",
+  marginLeft: "20px",
+  // marginBottom: "2rem",
+  // marginRight: "2rem",
+  // marginLeft: "2rem",
+  height: "50px",
+  textTransform: "none",
+  fontFamily: "Helvetica",
+  fontWeight: "bold",
+  backgroundColor: "rgba(0, 0, 0, 0.5)",
+};
 
 const imageTile = {
-    container: {
-        height: "25vh",
-        overflow: "hidden",
-        position: "relative",
-    },
-    image: {
-        height: "100%",
-        width: "100%",
-        objectFit: "cover",
-        opacity: "1.0",
-        display: "block",
-        transition: "0.5s ease",
-        backfaceVisibility: "hidden",
-        cursor: "pointer",
-    },
-    imageHover: {
-        filter: "brightness(50%)",
-    },
-    middle: {
-        transition: "0.5s ease",
-        opacity: 0,
-        position: "absolute",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-        msTransform: "translate(-50%, -50%)",
-        textAlign: "center",
-    },
+  container: {
+    height: "25vh",
+    overflow: "hidden",
+    position: "relative",
+  },
+  image: {
+    height: "100%",
+    width: "100%",
+    objectFit: "cover",
+    opacity: "1.0",
+    display: "block",
+    transition: "0.5s ease",
+    backfaceVisibility: "hidden",
+    cursor: "pointer",
+  },
+  imageHover: {
+    filter: "brightness(50%)",
+  },
+  middle: {
+    transition: "0.5s ease",
+    opacity: 0,
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    msTransform: "translate(-50%, -50%)",
+    textAlign: "center",
+  },
 };
 
 const header_styles = {
-    button_position,
-    button,
-    imageTile,
-  };
-  
-  export default header_styles;
+  button_position,
+  button,
+  imageTile,
+};
+
+export default header_styles;
--- ontour/src/components/Polaroid.jsx
+++ ontour/src/components/Polaroid.jsx
@@ -5,7 +5,7 @@ import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import polaroid_styles from "../Styles/polaroid_styles";
 import { Translate } from "@mui/icons-material";
 import { Button } from "@mui/material";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay } from "@fortawesome/free-solid-svg-icons";
 
 /* 
@@ -34,60 +34,53 @@ const Polaroid = (props) => {
             class="d-block w-100"
             style={polaroid_styles.polaroid_image}
             alt="..."
-          />                                                  
+          />
         </div>
       )}
       {props.videoUrl != null && (
         <>
-        <div
-                data-src1 = {props.videoUrl}
-                onClick={props.onPress}
-                style={{
-                  backgroundColor: isHovering ?  "rgba(0,0,0,.5)": "rgba(0,0,0)",
-                  color: "white",
-                  position: "absolute",
-                  top: "50%",
-                  left: "50%",
-                  transform: "translate(-50%,-50%)",
-                  zIndex: 1,
-                  width: "4rem",
-                  height: "3rem",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  borderRadius: "10px",
-                  cursor: "pointer",
-                  pointerEvents: "auto",
-                  transition: "background-color 0.3s, width 0.3s",
-                }}
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
+          <div
+            data-src1={props.videoUrl}
+            onClick={props.onPress}
+            style={{
+              backgroundColor: isHovering ? "rgba(0,0,0,.5)" : "rgba(0,0,0)",
+              color: "white",
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%,-50%)",
+              zIndex: 1,
+              width: "4rem",
+              height: "3rem",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: "10px",
+              // cursor: "pointer",
+              pointerEvents: "auto",
+              transition: "background-color 0.3s, width 0.3s",
+            }}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
           >
-           
-             
-                    &#9658;
-              {/* <FontAwesomeIcon data-src1 = {props.videoUrl}   icon={faPlay} /> */}
-              
-                 
-                
-            
-            </div>
+            &#9658;
+            {/* <FontAwesomeIcon data-src1 = {props.videoUrl}   icon={faPlay} /> */}
+          </div>
           <div
             onClick={props.onPress}
-            data-src1 = {props.videoUrl}
+            data-src1={props.videoUrl}
             style={polaroid_styles.polaroid_container}
           >
             <video
-            onClick={props.onPress}
-            data-src1 = {props.videoUrl}
+              onClick={props.onPress}
+              data-src1={props.videoUrl}
               autoplay
               playsInline
               preload="metadata"
-              src={props.videoUrl+"#t=0.2"}
+              src={props.videoUrl + "#t=0.2"}
               class="d-block w-100"
               style={polaroid_styles.polaroid_image}
             />
-            
           </div>
         </>
       )}
