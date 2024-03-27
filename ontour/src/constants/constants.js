c67ebaabdba9811c09642c8041964afeaba44304
deepanshutwotechies <deepanshu.twotechies@gmail.com>
2024-03-27 15:36:42 +0530

    mixpanel integeration done
    mixpanel integeration done

--- ontour/src/constants/constants.js
+++ ontour/src/constants/constants.js
@@ -2,4 +2,5 @@ export const adminEmail = ["deepanshu.twotechies@gmail.com","ben@tourscout.com",
 export const widthRatio = 3.68;
 export const heightRatio = .9;
 export const edgeFunctionImgUrl = "https://zouczoaamusrlkkuoppu.supabase.co/functions/v1/fetch-artist-image";
-export const mixPanelId = "046ea653daecc890e2168c762151eb85"
+export const mixPanelId = "046ea653daecc890e2168c762151eb85";
+export const mixPanelReportUrl = "https://zouczoaamusrlkkuoppu.supabase.co/functions/v1/pull_mixpanel_report"
--- ontour/src/pages/FanAnalytics.jsx
+++ ontour/src/pages/FanAnalytics.jsx
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import parse from "html-react-parser"
 import { html } from '../html';
+import { mixPanelReportUrl } from '../constants/constants';
 const FanAnalytics = () => {
+    const [html,setHtml] = useState(null)
+    const [show,setShow] = useState(false)
+    async function getHtml() {
+        try {
+            let res = await fetch(mixPanelReportUrl);
+            if (res.ok) {
+                let htmlText = await res.text();
+                console.log(htmlText)
+                setHtml(htmlText);
+            } else {
+                console.error('Failed to fetch HTML:', res.status, res.statusText);
+            }
+        } catch (error) {
+            console.error('Error fetching HTML:', error);
+        }
+    }
+    const load = ()=>{
+        setTimeout(()=>{
+            setShow(true)
+        },3000)
+    }
+    useEffect(() => {
+        // getHtml()
+    },[])
     return (
-     
-        <iframe style={{width:"90vw",height:"110vh",overflow:"hidden",scrollbarWidth:"none",}}  src="https://mixpanel.com/p/HNrJzwXEDnxmUipwP1Gmfw" frameborder="0"></iframe>
+      <div style={{position:"relative"}}>
+
+     <div style={{backgroundColor:"black",height:"12vh",width:"100%",position:"absolute",display:"flex",justifyContent:"center",alignItems:"center"}}>
+     <img style={{width:"5%",height:"50%",objectFit:"contain"}} src="../images/logo.png" alt="" />
+        <h1 style={{fontSize:"30px",marginTop:"20px", marginLeft:"10px",color:"white"}}>Fan Analytics</h1>
+
+     </div>
+
+        <iframe onLoadCapture={load}    id="iframe" style={{width:"99vw",height:"110vh",overflow:"hidden",scrollbarWidth:"none",display:show?"block":"none"}}  src="https://mixpanel.com/public/HNrJzwXEDnxmUipwP1Gmfw" frameborder="0">
+            
+        </iframe>
+        </div>
+        // <> {html && parse(html)}</>
+       
+
       
       
 
